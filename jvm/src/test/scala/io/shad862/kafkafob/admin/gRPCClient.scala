85748a7e732d3747c4d6af8add0af8b88bf30ac9
shad862 <shad862@gmail.com>
2020-10-03 05:11:31 +0300

    client adjusted

--- jvm/src/test/scala/io/shad862/kafkafob/admin/gRPCClient.scala
+++ jvm/src/test/scala/io/shad862/kafkafob/admin/gRPCClient.scala
@@ -2,26 +2,19 @@ package io.shad862.kafkafob.admin
 
 import akka.actor.ActorSystem
 import akka.grpc.GrpcClientSettings
-import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
 import io.shad862.kafkafob.admin.proto.{DescribeGroupsRequest, DescribeGroupsServiceClient}
-
-import scala.concurrent.Await._
-import scala.concurrent.duration._
 
 
 object gRPCClient {
   def main(args: Array[String]): Unit = {
 
     implicit val system = ActorSystem("HelloWorldClient")
-    implicit val materializer = ActorMaterializer()
     implicit val contextExecutor = system.dispatcher
 
-    val clientSettings = GrpcClientSettings.connectToServiceAt("127.0.0.1", 8080).withTls(false)
+    val client: DescribeGroupsServiceClient = DescribeGroupsServiceClient(GrpcClientSettings.connectToServiceAt("127.0.0.1", 8080).withTls(false))
 
-    val client: DescribeGroupsServiceClient = DescribeGroupsServiceClient(clientSettings)
-    val response = result(client.describeGroups(DescribeGroupsRequest()), Duration.Inf)
-    println(response)
-
-    //client.describeGroupsReplying(DescribeGroupsRequest()).to(Sink.foreach(println))
+    client.describeGroups(DescribeGroupsRequest()).onComplete(println(_))
+    client.describeGroupsReplying(DescribeGroupsRequest()).runWith(Sink.foreach(println(_)))
   }
 }
